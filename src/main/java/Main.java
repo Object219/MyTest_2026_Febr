e31c40d4997ff9203f8a658c3370222af4a386ba
Professional <object219@bk.ru>
2026-02-09 01:12:20 +0300

    Chrome COMM

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,18 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
 
 public class Main {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver","C:\\Program Files\\chromedriver_win32\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","C:\\Program Files\\chromedriver-win64\\chromedriver.exe");
         WebDriver driver=new ChromeDriver();
 
         driver.get("https://google.com");
+
+        WebElement input= driver.findElement(By.xpath("//input[@aria-label='Поиск в Google']"));
+
+        input.click();
 
     }
 }
